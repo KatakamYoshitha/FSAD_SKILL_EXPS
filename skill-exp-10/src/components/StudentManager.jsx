c791b88762fe36779f791150ab11f3c9f32dab4b
Katakam Yoshitha <yoshithaka123@gmail.com>
2026-03-15 14:51:55 +0530

    added 10 exp

---
+++ skill-exp-10/src/components/StudentManager.jsx
@@ -0,0 +1,80 @@
+import React,{useState} from "react";
+import "./StudentManager.css";
+
+function StudentManager(){
+const initialStudents=[
+{id:2400031000,name:"Rahul",course:"CSE"},
+{id:2400031001,name:"Sneha",course:"AI&DS"},
+{id:2400031002,name:"Arjun",course:"IT"},
+{id:2400031003,name:"Priya",course:"CS"},
+{id:2400031004,name:"Kiran",course:"CSE"}
+];
+
+const [students,setStudents]=useState(initialStudents);
+
+const [newStudent,setNewStudent]=useState({
+id:"",
+name:"",
+course:""
+});
+
+const handleChange=(e)=>{
+const {name,value}=e.target;
+setNewStudent({...newStudent,[name]:value});
+};
+
+const addStudent=()=>{
+setStudents([...students,newStudent]);
+setNewStudent({id:"",name:"",course:""});
+};
+
+const deleteStudent=(id)=>{
+const updated=students.filter((student)=>student.id!==id);
+setStudents(updated);
+};
+
+return(
+
+<div className="container">
+<h2>Student Manager</h2>
+
+<div className="form">
+<input type="number" name="id" placeholder="ID" value={newStudent.id} onChange={handleChange}/>
+<input type="text" name="name" placeholder="Name" value={newStudent.name} onChange={handleChange}/>
+<input type="text" name="course" placeholder="Course" value={newStudent.course} onChange={handleChange}/>
+<button onClick={addStudent}>Add Student</button>
+</div>
+
+{students.length===0?(
+
+<p>No students available</p>
+):(
+<table>
+<thead>
+<tr>
+<th>ID</th>
+<th>Name</th>
+<th>Course</th>
+<th>Action</th>
+</tr>
+</thead>
+
+<tbody>
+{students.map((s)=>(
+<tr key={s.id}>
+<td>{s.id}</td>
+<td>{s.name}</td>
+<td>{s.course}</td>
+<td>
+<button onClick={()=>deleteStudent(s.id)}>Delete</button>
+</td>
+</tr>
+))}
+</tbody>
+</table>
+)}
+</div>
+);
+}
+
+export default StudentManager;
